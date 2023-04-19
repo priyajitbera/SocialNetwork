872fb67e4c2971b7bccd0107e3ffcbee549c8b0e
Priyajit Bera <priyajitbera@gmail.com>
2023-04-19 12:19:37 +0530

    first commit

---
+++ src/main/java/com/priyajit/project/socialnetwork/SocialnetworkApplication.java
@@ -0,0 +1,19 @@
+package com.priyajit.project.socialnetwork;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class SocialnetworkApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialnetworkApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	}
+}
