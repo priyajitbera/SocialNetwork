e0f3d5ec0c7c311f12d60da0ebffdc4841c67483
Priyajit Bera <priyajitbera@gmail.com>
2023-04-21 21:09:48 +0530

    add security config

--- src/main/java/com/priyajit/project/socialnetwork/config/SecurityConfig.java
+++ src/main/java/com/priyajit/project/socialnetwork/config/SecurityConfig.java
@@ -1,7 +1,15 @@
 package com.priyajit.project.socialnetwork.config;
 
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Component
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
