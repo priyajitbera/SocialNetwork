64ebcb6c8c57d63496793ec396541cdc701d24f9
Priyajit Bera <priyajitbera@gmail.com>
2023-04-21 21:12:03 +0530

    call command line client

--- src/main/java/com/priyajit/project/socialnetwork/SocialnetworkApplication.java
+++ src/main/java/com/priyajit/project/socialnetwork/SocialnetworkApplication.java
@@ -1,13 +1,24 @@
 package com.priyajit.project.socialnetwork;
 
+import com.priyajit.project.socialnetwork.client.ApplicationClient;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Scanner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SocialnetworkApplication implements CommandLineRunner {
+
+	private final ApplicationClient applicationClient;
+
+	@Autowired
+	public SocialnetworkApplication(ApplicationClient applicationClient) {
+		this.applicationClient = applicationClient;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialnetworkApplication.class, args);
@@ -15,5 +26,6 @@ public class SocialnetworkApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		applicationClient.start();
 	}
 }
